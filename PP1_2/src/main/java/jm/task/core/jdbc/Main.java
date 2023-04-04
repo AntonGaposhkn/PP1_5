bb5f3d27a71522b6ec25588d4937b95f5da1fe63
AntonGaposhkn <122727304+AntonGaposhkn@users.noreply.github.com>
2023-04-04 14:20:21 +0300

    Add files via upload

---
+++ PP1_2/src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,20 @@
+package jm.task.core.jdbc;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+public class Main {
+    public static void main(String[] args) {
+
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Igor", "Ivanov", (byte) 20);
+        userService.saveUser("Ivan", "Olegov", (byte) 25);
+        userService.saveUser("Oleg", "Alexov", (byte) 31);
+        userService.saveUser("Alex", "Igorov", (byte) 38);
+        userService.removeUserById(6);
+        System.out.println(userService.getAllUsers());
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
+    }
+}
